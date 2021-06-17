abd56f09feb8943f746d404d9cf7ee4b2fee7817
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-06-17 12:49:21 +0200

    Upd engines

--- engines.py
+++ engines.py
@@ -1,5 +1,4 @@
 from talon import speech_system
 from talon.engines.w2l import W2lEngine
-# w2l = W2lEngine(model='en_US', debug=False)
-w2l = W2lEngine(model='en_US-conformer', debug=True)
+w2l = W2lEngine(model="en_US-conformer", debug=True)
 speech_system.add_engine(w2l)
