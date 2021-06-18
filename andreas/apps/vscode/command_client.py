ffde31311130fb0bda8fd1cfe650b18683a57496
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-06-18 08:55:52 +0200

    merge more robust command client

--- andreas/apps/vscode/command_client.py
+++ andreas/apps/vscode/command_client.py
@@ -1,15 +1,13 @@
-import getpass
-from dataclasses import dataclass
 import json
 import os
 import time
+from dataclasses import dataclass
 from pathlib import Path
 from tempfile import gettempdir
 from typing import Any, List
 from uuid import uuid4
 
-from talon import Module, actions, Context
-
+from talon import Context, Module, actions
 
 # How old a request file needs to be before we declare it stale and are willing
 # to remove it
@@ -50,13 +48,13 @@ def run_vscode_command_by_command_palette(command_id: str):
 
 def write_json_exclusive(path: Path, body: Any):
     """Writes jsonified object to file, failing if the file already exists
+
     Args:
         path (Path): The path of the file to write
         body (Any): The object to convert to json and write
     """
     with path.open("x") as out_file:
         out_file.write(json.dumps(body))
-
 
 
 @dataclass
@@ -80,9 +78,11 @@ class Request:
 def write_request(request: Request, path: Path):
     """Converts the given request to json and writes it to the file, failing if
     the file already exists unless it is stale in which case it replaces it
+
     Args:
         request (Request): The request to serialize
         path (Path): The path to write to
+
     Raises:
         Exception: If another process has an active request file
     """
@@ -95,7 +95,6 @@ def write_request(request: Request, path: Path):
     if request_file_exists:
         handle_existing_request_file(path)
         write_json_exclusive(path, request.to_dict())
-
 
 
 def handle_existing_request_file(path):
@@ -114,7 +113,7 @@ def handle_existing_request_file(path):
             raise Exception("Found recent request file; vscode is probably hung")
     else:
         print("Removing stale request file")
-        path.unlink()
+        robust_unlink(path)
 
 
 def run_vscode_command(
@@ -124,21 +123,22 @@ def run_vscode_command(
     return_command_output: bool = False,
 ):
     """Runs a VSCode command, using command server if available
+
     Args:
         command_id (str): The ID of the VSCode command to run
         wait_for_finish (bool, optional): Whether to wait for the command to finish before returning. Defaults to False.
         return_command_output (bool, optional): Whether to return the output of the command. Defaults to False.
+
     Raises:
         Exception: If there is an issue with the file-based communication, or
         VSCode raises an exception
+
     Returns:
         Object: The response from the command, if requested.
     """
     # NB: This is a hack to work around the fact that talon doesn't support
     # variable argument lists
     args = [x for x in args if x is not NotSet]
-
-    username = getpass.getuser()
 
     communication_dir_path = get_communication_dir_path()
 
@@ -156,8 +156,6 @@ def run_vscode_command(
     # sanity checking
     uuid = str(uuid4())
 
-    unlink_if_exists(response_path)
-
     request = Request(
         command_id=command_id,
         args=args,
@@ -166,20 +164,28 @@ def run_vscode_command(
         uuid=uuid,
     )
 
-    # First, write the request to the request file
+    # First, write the request to the request file, which makes us the sole
+    # owner because all other processes will try to open it with 'x'
     write_request(request, request_path)
+
+    # We clear the response file if it does exist, though it shouldn't
+    if response_path.exists():
+        print("WARNING: Found old response file")
+        robust_unlink(response_path)
 
     # Then, perform keystroke telling VSCode to execute the command in the
     # request file.  Because only the active VSCode instance will accept
     # keypresses, we can be sure that the active VSCode instance will be the
     # one to execute the command.
-
     actions.user.trigger_command_server_command_execution()
 
     try:
         decoded_contents = read_json_with_timeout(response_path)
     finally:
-        unlink_if_exists(response_path)
+        # NB: We remove response file first because we want to do this while we
+        # still own the request file
+        robust_unlink(response_path)
+        robust_unlink(request_path)
 
     if decoded_contents["uuid"] != uuid:
         raise Exception("uuids did not match")
@@ -194,6 +200,7 @@ def run_vscode_command(
 
 def get_communication_dir_path():
     """Returns directory that is used by command-server for communication
+
     Returns:
         Path: The path to the communication dir
     """
@@ -207,20 +214,40 @@ def get_communication_dir_path():
     return Path(gettempdir()) / f"vscode-command-server{suffix}"
 
 
-def unlink_if_exists(path):
+def robust_unlink(path: Path):
+    """Unlink the given file if it exists, and if we're on windows and it is
+    currently in use, just rename it
+
+    Args:
+        path (Path): The path to unlink
+    """
     try:
-        path.unlink()
-    except FileNotFoundError:
-        pass
+        path.unlink(missing_ok=True)
+    except OSError as e:
+        if hasattr(e, "winerror") and e.winerror == 32:
+
+            graveyard_dir = get_communication_dir_path() / "graveyard"
+            graveyard_dir.mkdir(parents=True, exist_ok=True)
+            graveyard_path = graveyard_dir / str(uuid4())
+            print(
+                f"WARNING: File {path} was in use when we tried to delete it; "
+                f"moving to graveyard at path {graveyard_path}"
+            )
+            path.rename(graveyard_path)
+        else:
+            raise e
 
 
 def read_json_with_timeout(path: str) -> Any:
     """Repeatedly tries to read a json object from the given path, waiting
     until there is a trailing new line indicating that the write is complete
+
     Args:
         path (str): The path to read from
+
     Raises:
         Exception: If we timeout waiting for a response
+
     Returns:
         Any: The json-decoded contents of the file
     """
@@ -264,7 +291,7 @@ class Actions:
         finish."""
         run_vscode_command(command_id, wait_for_finish=True)
 
-    # def vscode_with_plugin(
+    #def vscode_with_plugin(
     def vscode(
         command_id: str,
         arg1: Any = NotSet,
