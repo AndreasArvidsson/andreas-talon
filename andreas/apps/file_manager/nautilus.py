da9f3a75a65bf5487fb82810277d8bbf23e0ff3c
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2021-06-21 20:01:58 +0200

    added nautilus

---
+++ andreas/apps/file_manager/nautilus.py
@@ -0,0 +1,41 @@
+from talon import Context, Module, actions, app
+key = actions.key
+insert = actions.insert
+
+mod = Module()
+
+mod.apps.nautilus = """
+os: linux
+and app.name: Org.gnome.Nautilus
+"""
+
+ctx = Context()
+ctx.matches = """
+app: nautilus
+"""
+
+ctx.tags = ["user.file_manager", "user.tabs"]
+
+
+@ctx.action_class("user")
+class UserActions:
+    def file_manager_go_back():         key("alt-left")
+    def file_manager_go_forward():      key("alt-right")
+    def file_manager_go_parent():       key("alt-up")
+    def file_manager_focus_address():   key("ctrl-l")
+    def file_manager_show_properties(): key("ctrl-i")
+
+    def file_manager_copy_address():
+        actions.user.file_manager_focus_address()
+        actions.edit.copy()
+        key("escape")
+
+    def file_manager_open_directory(path: str):
+        key("ctrl-l")
+        insert(path)
+        key("enter")
+
+    def file_manager_new_folder(name: str = None):
+        key("ctrl-shift-n")
+        if name:
+            insert(name)
