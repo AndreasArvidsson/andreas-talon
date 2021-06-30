bb708c2ef216079c97e651d9089c094958af7f6b
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2021-06-30 16:54:50 +0200

    Added abort

---
+++ andreas/misc/abort/abort.py
@@ -0,0 +1,9 @@
+from talon import Module, actions, speech_system, app
+
+def on_pre_phrase(d):
+    words = d["parsed"]._unmapped
+    if words[-1] == "abort" and actions.speech.enabled():
+        app.notify("Command aborted: " + " ".join(d["phrase"]))
+        d["parsed"]._sequence = []
+
+speech_system.register("pre:phrase", on_pre_phrase)
