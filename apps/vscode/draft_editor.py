41bf2ccc971bcd67306f0318b49c1ecb71d5305e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-07-09 17:24:42 +0200

    updated draft editor to preserve clipboard

--- apps/vscode/draft_editor.py
+++ apps/vscode/draft_editor.py
@@ -1,7 +1,7 @@
 from talon import Module, actions, ui
 
 mod = Module()
-mod.mode("draft_editor", "Mode to show if the draft editor is open")
+mod.mode("draft_editor", "Indicates whether the draft editor has been activated")
 
 active_window = None
 
@@ -12,22 +12,23 @@ class Actions:
         global active_window
         active_window = ui.active_window()
         editor = get_editor()
-        has_selected_text = actions.edit.selected_text() != ""
-        if has_selected_text:
-            actions.edit.copy()
+        selected_text = actions.edit.selected_text()
         actions.user.focus_window(editor)
-        actions.user.vscode("workbench.action.files.newUntitledFile")
-        if has_selected_text:
-            actions.edit.paste()
+        # Wait for context to change.
+        actions.sleep("100ms")
+        actions.app.tab_open()
+        if selected_text != "":
+            actions.user.paste(selected_text)
         actions.mode.enable("user.draft_editor")
 
     def draft_editor_submit():
-        """Submit/save draft"""
+        """Submit/save draft editor"""
         close_editor(submit_draft=True)
 
     def draft_editor_discard():
-        """Discard draft"""
+        """Discard draft editor"""
         close_editor(submit_draft=False)
+
 
 def get_editor():
     editor_names = {
@@ -45,10 +46,9 @@ def get_editor():
 def close_editor(submit_draft: bool):
     actions.mode.disable("user.draft_editor")
     actions.edit.select_all()
-    if submit_draft:
-        actions.edit.copy()
+    selected_text = actions.edit.selected_text()
     actions.edit.delete()
     actions.app.tab_close()
     actions.user.focus_window(active_window)
     if submit_draft:
-        actions.edit.paste()
+        actions.user.paste(selected_text)
