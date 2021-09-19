6b736cd0c93bc33632ca0be1007586ac5162d463
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-09-19 09:58:38 +0200

    Updated engines.py for new internally downloaded conformer

--- engines.py
+++ engines.py
@@ -2,8 +2,8 @@ from talon import Context, speech_system
 from talon.engines.w2l import W2lEngine
 from talon.engines.webspeech import WebSpeechEngine
 
-w2l = W2lEngine(model="en_US-conformer", debug=True)
-speech_system.add_engine(w2l)
+# w2l = W2lEngine(model="en_US-conformer", debug=True)
+# speech_system.add_engine(w2l)
 
 webspeech = WebSpeechEngine()
 speech_system.add_engine(webspeech)
