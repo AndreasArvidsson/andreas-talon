635a8737221d8c47ee0b1207dda76a1d3761f834
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-11-27 14:08:15 +0100

    Updated settings for new screen

--- settings/settings.py
+++ settings/settings.py
@@ -1,5 +1,5 @@
 from talon.types import Point2d
 from talon_plugins import eye_zoom_mouse
 
-eye_zoom_mouse.config.screen_area = Point2d(300, 200)
+eye_zoom_mouse.config.screen_area = Point2d(350, 250)
 eye_zoom_mouse.config.img_scale = 5
--- settings/settings.talon
+++ settings/settings.talon
@@ -1,6 +1,6 @@
 settings():
     # Adjust the scale of the imgui to my liking
-    imgui.scale = 1.3
+    imgui.scale = 2
 
     # Max number of command lines per page in help
     user.help_max_command_lines_per_page = 50
