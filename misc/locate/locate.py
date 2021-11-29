c2a51a188abecea23af818510403f3667865fa59
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-11-29 08:22:57 +0100

    Added guard if location was not found

--- misc/locate/locate.py
+++ misc/locate/locate.py
@@ -7,7 +7,7 @@ mod = Module()
 
 @mod.action_class
 class Actions:
-    def locate_hover(name: str) -> tuple[int, int]:
+    def locate_hover(name: str) -> tuple[bool, int, int]:
         """Hover image"""
         path = f"{actions.path.talon_user()}/andreas/images/{name}"
         rect = ui.active_window().rect
@@ -16,23 +16,27 @@ class Actions:
         print(f"{time.perf_counter() - t1:0.3}s")
         print(f"{len(locations)}x")
 
+        if len(locations) == 0:
+            return False, 0, 0
+
         location = locations[0]
         x = round(rect.x + location.x + location.width / 2)
         y = round(rect.y + location.y + location.height / 2)
         ctrl.mouse_move(x, y)
-        return x, y
+        return True, x, y
 
-    def locate_grab_line(direction: str) -> tuple[int, int]:
+    def locate_grab_line(direction: str) -> tuple[bool, int, int]:
         """Locate and grab line"""
-        x, y = actions.user.locate_hover(f"line_{direction}.png")
-        ctrl.mouse_click(button=0, down=True)
-        return x, y
+        ok, x, y = actions.user.locate_hover(f"line_{direction}.png")
+        if ok:
+            ctrl.mouse_click(button=0, down=True)
+        return ok, x, y
 
     def locate_line_move(direction: int):
         """Move line"""
-        x, y = actions.user.locate_grab_line("vertical")
-        actions.sleep(0.2)
-        ctrl.mouse_move(x + 100 * direction, y)
-        actions.sleep(0.2)
-        ctrl.mouse_click(button=0, up=True)
-
+        ok, x, y = actions.user.locate_grab_line("vertical")
+        if ok:
+            actions.sleep(0.2)
+            ctrl.mouse_move(x + 100 * direction, y)
+            actions.sleep(0.2)
+            ctrl.mouse_click(button=0, up=True)
