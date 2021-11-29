f05cee7f006dd01f2ec30eb2c2f3e549f9911ff0
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-11-29 08:10:34 +0100

    Updated location with grab line

--- misc/locate/locate.py
+++ misc/locate/locate.py
@@ -1,21 +1,38 @@
 from talon import Module, actions, ui, ctrl
 from talon.experimental.locate import locate
+import time
 
 mod = Module()
 
 
 @mod.action_class
 class Actions:
-    def locate_hover(name: str):
+    def locate_hover(name: str) -> tuple[int, int]:
         """Hover image"""
         path = f"{actions.path.talon_user()}/andreas/images/{name}"
         rect = ui.active_window().rect
-        locations = locate(path, rect=rect)
-
-        for l in locations:
-            print(f"{rect.x+l.x}, {rect.y+l.y}, {l.width}, {l.height}")
+        t1 = time.perf_counter()
+        locations = locate(path, rect=rect, threshold=0.99)
+        print(f"{time.perf_counter() - t1:0.3}s")
+        print(f"{len(locations)}x")
 
         location = locations[0]
-        x = rect.x + location.x + location.width / 2
-        y = rect.y + location.y + location.height / 2
+        x = round(rect.x + location.x + location.width / 2)
+        y = round(rect.y + location.y + location.height / 2)
         ctrl.mouse_move(x, y)
+        return x, y
+
+    def locate_grab_line(direction: str) -> tuple[int, int]:
+        """Locate and grab line"""
+        x, y = actions.user.locate_hover(f"line_{direction}.png")
+        ctrl.mouse_click(button=0, down=True)
+        return x, y
+
+    def locate_line_move(direction: int):
+        """Move line"""
+        x, y = actions.user.locate_grab_line("vertical")
+        actions.sleep(0.2)
+        ctrl.mouse_move(x + 100 * direction, y)
+        actions.sleep(0.2)
+        ctrl.mouse_click(button=0, up=True)
+
--- misc/locate/locate.talon
+++ misc/locate/locate.talon
@@ -1,2 +1,5 @@
-locate button:   user.locate_hover("button.png")
-locate line:     user.locate_hover("line.png")
+locate button:     user.locate_hover("button.png")
+grab line left:    user.locate_grab_line("vertical")
+grab line down:    user.locate_grab_line("horizontal")
+move line left:    user.locate_line_move(-1)
+move line right:   user.locate_line_move(1)
