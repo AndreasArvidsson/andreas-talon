5381a0baac572e1cce4b17a888ca9fd070053372
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-09 15:16:52 +0100

    Rename Swedish words to Swedish phrases

--- text/text_swedish.py
+++ text/text_swedish.py
@@ -3,8 +3,8 @@ from talon import Module, Context
 mod = Module()
 ctx = Context()
 
-mod.list("swedish_word", desc="List of Swedish words")
-ctx.lists["self.swedish_word"] = {
+mod.list("swedish_phrase", desc="List of Swedish phrases")
+ctx.lists["self.swedish_phrase"] = {
     "hello": "Hej",
     "goodbye": "Hejdå",
     "thanks": "Tack",
