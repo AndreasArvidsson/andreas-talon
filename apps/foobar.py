9f8571dc7657f92df9c821faf1ce27421afee1bd
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-10 19:15:47 +0100

    Updated foobar commands

--- apps/foobar.py
+++ apps/foobar.py
@@ -1,4 +1,5 @@
 from talon import Module, Context, actions
+
 key = actions.key
 
 mod = Module()
@@ -18,7 +19,16 @@ app: foobar
 ctx.tags = ["user.tabs"]
 
 
+@ctx.action_class("app")
+class AppActions:
+    def preferences():
+        key("ctrl-p")
+
+
 @ctx.action_class("user")
 class UserActions:
-    def volume_up():    key("ctrl-up")
-    def volume_down():  key("ctrl-down")
+    def volume_up():
+        key("ctrl-up")
+
+    def volume_down():
+        key("ctrl-down")
