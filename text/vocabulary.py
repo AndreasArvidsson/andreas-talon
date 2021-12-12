f12859d7c9fc182653895b620ec6b2533d644871
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-12 03:22:09 +0100

    Updated vocabulary

--- text/vocabulary.py
+++ text/vocabulary.py
@@ -84,7 +84,8 @@ simple_default_vocabulary = [
     "yeah",
     "usb",
     "highcharts",
-    "csv"
+    "csv",
+    "chuck",
 ]
 # Defaults for different pronounciations of words that need to be added to Talon's vocabulary.
 default_vocabulary = {
