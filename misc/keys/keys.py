ea0652f58212fe100f6f198b432ef9b835177d98
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-19 00:18:11 +0100

    Added urn key

--- misc/keys/keys.py
+++ misc/keys/keys.py
@@ -7,7 +7,7 @@ mod.tag("keys")
 
 # alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(" ")
 # alphabet = "air batt cap drum each fine gust harp ink jig kid look made near ox pit quench ram soon trap urge vest whale plex yank zip".split(
-alphabet = "air batt cap drum each fine gust harp ink jig kid look made near ox pit quench ram soon trap ursa vest whale plex yank zip".split(
+alphabet = "air batt cap drum each fine gust harp ink jig kid look made near ox pit quench ram soon trap urn vest whale plex yank zip".split(
     " ")
 default_digits = "zero one two three four five six seven eight nine ten eleven twelve".split(
     " ")
