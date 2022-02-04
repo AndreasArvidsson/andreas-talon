572119d5e3b20351ea04b4c3c79ef0615918b7ce
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-02-04 12:15:23 +0100

    Added Microsoft teams

---
+++ apps/web/teams.py
@@ -0,0 +1,21 @@
+from talon import Context, Module, actions
+
+ctx = Context()
+mod = Module()
+
+mod.apps.teams = """
+tag: browser
+and title: /Microsoft Teams/
+"""
+
+ctx.matches = r"""
+app: teams
+"""
+
+ctx.tags = ["user.av"]
+
+
+@ctx.action_class("user")
+class UserActions:
+    def mute_microphone():
+        actions.key("ctrl-shift-m")
