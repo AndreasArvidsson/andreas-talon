4f022f65ec3e365bfea36fadb7314c623eda442b
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-02-10 10:11:38 +0100

    Added Facebook application

---
+++ apps/web/facebook.py
@@ -0,0 +1,22 @@
+from talon import Context, actions, Module
+
+ctx = Context()
+mod = Module()
+
+mod.apps.facebook = """
+tag: browser
+and title: /Facebook/
+"""
+
+ctx.matches = """
+app: facebook
+"""
+
+
+@ctx.action_class("edit")
+class EditActions:
+    def line_insert_up():
+        actions.key("home ctrl-enter up")
+
+    def line_insert_down():
+        actions.key("end ctrl-enter")
