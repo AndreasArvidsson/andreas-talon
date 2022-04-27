d643f736fa9c4445dc9dc93678d43c2d4bc47325
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-04-27 17:06:36 +0200

    Added hiss noise

---
+++ misc/on_hiss.py
@@ -0,0 +1,20 @@
+from talon import noise, actions, cron
+
+cron_job = None
+
+
+def on_hiss(active: bool):
+    global cron_job
+    if actions.speech.enabled():
+        if active:
+            cron_job = cron.after("100ms", mouse_scrolling)
+        else:
+            cron.cancel(cron_job)
+            actions.user.mouse_stop()
+
+
+def mouse_scrolling():
+    actions.user.mouse_scrolling("down")
+
+
+noise.register("hiss", on_hiss)
