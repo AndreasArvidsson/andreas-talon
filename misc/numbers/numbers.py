3340e052799d74ee533bb8bc51c1c246a7a8dce8
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-05-30 20:03:00 +0200

    Added float number capture

--- misc/numbers/numbers.py
+++ misc/numbers/numbers.py
@@ -1,3 +1,4 @@
+# fmt: off
 from talon import Context, Module
 from typing import Union, Iterator
 
@@ -173,10 +174,19 @@ def number(m) -> int:
 def number_small(m): 
     return int(parse_number(list(m)))
 
-@mod.capture(rule="(numb | number) <user.number_string>")
+@mod.capture(rule="<user.number_string> point <user.number_string>")
+def number_float_string(m) -> str:
+    """Parses a float number phrase, returning that number as a string."""
+    return f"{m.number_string_1}.{m.number_string_2}"
+
+
+@mod.capture(rule="(numb | number) (<user.number_string> | <user.number_float_string>)")
 def number_prefix(m) -> str:
     """Parses a prefixed number phrase, returning that number as a string."""
-    return m.number_string
+    try:
+        return m.number_string
+    except AttributeError:
+        return m.number_float_string
 
 @mod.capture(rule=f"{'|'.join(digits[1:])}")
 def digit(m) -> int:
