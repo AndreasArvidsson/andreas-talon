6681cbbb76bc24185243638e5fb163ff104faa64
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-06-01 07:52:26 +0200

    Added snap bottom center and top center commands

--- misc/window_management/window_snap.py
+++ misc/window_management/window_snap.py
@@ -66,8 +66,10 @@ snap_positions = {
     "bottom right large": RelativeScreenPos(1 / 3, 0.5, 1, 1),
     "bottom center small": RelativeScreenPos(1 / 3, 0.5, 2 / 3, 1),
     # Special
-    "middle": RelativeScreenPos(1 / 6, 1 / 8, 5 / 6, 1),
     "center": RelativeScreenPos(1 / 6, 0, 5 / 6, 1),
+    "top center": RelativeScreenPos(1 / 6, 0, 5 / 6, 0.5),
+    "bottom center": RelativeScreenPos(1 / 6, 0.5, 5 / 6, 1),
+    "middle": RelativeScreenPos(1 / 6, 1 / 8, 5 / 6, 1),
     "full": RelativeScreenPos(0, 0, 1, 1),
 }
 
