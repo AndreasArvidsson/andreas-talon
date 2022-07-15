9b1d13c533f9f5e79b218c5616e322889a74dba4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-07-15 09:08:13 +0200

    Updated file manager to always use system default path

--- apps/file_manager/file_manager.py
+++ apps/file_manager/file_manager.py
@@ -14,15 +14,19 @@ class Actions:
     def talon_app() -> str:
         """Get path to talon application"""
         return actions.path.talon_app()
+
     def talon_home() -> str:
         """Get path to talon home"""
         return actions.path.talon_home()
+
     def talon_user() -> str:
         """Get path to talon user"""
         return actions.path.talon_user()
+
     def user_home() -> str:
         """Get path to user home"""
         return actions.path.user_home()
+
     def update_path(path: str):
         """Update given path"""
         return path
@@ -30,60 +34,65 @@ class Actions:
     # ----- Navigation -----
     def file_manager_go_parent():
         """File manager go parent"""
+
     def file_manager_focus_address():
         """File manager focus address field"""
+
     def file_manager_open_directory(path: str):
         """File manager go to path"""
+
     def file_manager_copy_address():
         """File manager copy address"""
 
     # ----- Create folders / files -----
     def file_manager_new_folder(name: str = None):
         """Creates a new folder"""
+
     def file_manager_new_file(name: str = None):
         """Creates a new file"""
 
     # ----- Miscellaneous -----
     def file_manager_show_properties():
         """Shows the properties for the file"""
+
     def file_manager_terminal_here():
         """Opens terminal at current location"""
 
 
 def get_windows_paths():
     return {
-        "recycle bin":          "shell:RecycleBinFolder",
-        "applications":         "shell:AppsFolder",
-        "root":                 environ["HOMEDRIVE"],
-        "app data":             environ["APPDATA"],
-        "program files":        environ["PROGRAMFILES"],
-        "temp":                 environ["TEMP"],
-        "windows":              environ["WINDIR"]
+        "recycle bin": "shell:RecycleBinFolder",
+        "applications": "shell:AppsFolder",
+        "root": environ["HOMEDRIVE"],
+        "app data": environ["APPDATA"],
+        "program files": environ["PROGRAMFILES"],
+        "temp": environ["TEMP"],
+        "windows": environ["WINDIR"],
     }
 
 
 def get_linux_paths():
     return {
-        "root":                 "/",
-        "temp":                "/tmp"
+        "root": "/",
+        "temp": "/tmp",
     }
 
 
 def on_ready():
-    user_path = str(actions.user.user_home())
+    user_path = str(actions.path.user_home())
     user_dirs = [
         "Desktop",
         "Documents",
         "Downloads",
         "Pictures",
         "Videos",
-        "Dropbox"
+        "Dropbox",
     ]
     common_paths = {
-        "user":                 user_path,
-        "talon app":            str(actions.user.talon_app()),
-        "talon home":           str(actions.user.talon_home()),
-        "talon user":           str(actions.user.talon_user())
+        "user": user_path,
+        "talon app": str(actions.path.talon_app()),
+        "talon home": str(actions.path.talon_home()),
+        "talon user": str(actions.path.talon_user()),
     }
     os_paths = {}
     if app.platform == "windows":
@@ -92,9 +101,7 @@ def on_ready():
         os_paths = get_linux_paths()
 
     ctx.lists["self.path"] = merge(
-        {p.lower(): path.join(user_path, p) for p in user_dirs},
-        common_paths,
-        os_paths
+        {p.lower(): path.join(user_path, p) for p in user_dirs}, common_paths, os_paths
     )
 
     # for k, v in sorted(ctx.lists["self.path"].items(), key=lambda i: i[1]):
