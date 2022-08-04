d87673838f3593863645b5096e36c1cde086d12e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-08-04 15:36:54 +0200

    Added update script

---
+++ update.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+const childProcess = require("child_process");
+
+function getUserPath() {
+  const parentDir = path.dirname(__dirname);
+  const pathParts = parentDir.split(path.sep);
+  if (pathParts.at(-1) !== "user" || pathParts.at(-2) !== "talon") {
+    throw Error(`Can't find talon user directory from path: ${__dirname}`);
+  }
+  return parentDir;
+}
+
+function gitUpdate(userDir, url, folderName, branch) {
+  const repositoryDir = path.join(userDir, folderName);
+  if (fs.existsSync(repositoryDir)) {
+    console.log(`'${folderName}' already exists`);
+  } else {
+    childProcess.execSync(`git clone ${url} ${folderName}`, { cwd: userDir });
+  }
+  childProcess.execSync(`git checkout ${branch}`, { cwd: repositoryDir });
+  childProcess.execSync(`git pull`, { cwd: repositoryDir });
+  console.log();
+}
+
+(() => {
+  const userDir = getUserPath();
+  console.log(`Talon user directory: ${userDir}\n`);
+
+  gitUpdate(
+    userDir,
+    "git@github.com:AndreasArvidsson/andreas-talon.git",
+    "andreas",
+    "master"
+  );
+
+  gitUpdate(
+    userDir,
+    "git@github.com:AndreasArvidsson/talon-vscode-command-client.git",
+    "command-client",
+    "master"
+  );
+
+  gitUpdate(
+    userDir,
+    "git@github.com:cursorless-dev/cursorless-talon.git",
+    "cursorless-talon",
+    "andreas"
+  );
+
+  gitUpdate(
+    userDir,
+    "git@github.com:AndreasArvidsson/rango-talon.git",
+    "rango-talon",
+    "main"
+  );
+})();
