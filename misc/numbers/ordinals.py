496de7ced6b4917c6772cd1f84e5cf4a19d7a13f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-07 08:49:55 +0200

    Clean up

--- misc/numbers/ordinals.py
+++ misc/numbers/ordinals.py
@@ -1,21 +1,6 @@
 from talon import Context, Module
 
 
-def ordinal(n):
-    """
-    Convert an integer into its ordinal representation::
-        ordinal(0)   => '0th'
-        ordinal(3)   => '3rd'
-        ordinal(122) => '122nd'
-        ordinal(213) => '213th'
-    """
-    n = int(n)
-    suffix = ["th", "st", "nd", "rd", "th"][min(n % 10, 4)]
-    if 11 <= (n % 100) <= 13:
-        suffix = "th"
-    return str(n) + suffix
-
-
 # The primitive ordinal words in English below a hundred.
 ordinal_words = {
     0: "zeroth",
@@ -52,6 +37,7 @@ tens_words = "zero ten twenty thirty forty fifty sixty seventy eighty ninety".sp
 # ordinal_numbers maps ordinal words into their corresponding numbers.
 ordinal_numbers = {}
 ordinal_small = {}
+
 for n in range(1, 100):
     if n in ordinal_words:
         word = ordinal_words[n]
@@ -60,7 +46,6 @@ for n in range(1, 100):
         assert 1 < tens < 10, "we have already handled all ordinals < 20"
         assert 0 < units, "we have already handled all ordinals divisible by ten"
         word = f"{tens_words[tens]} {ordinal_words[units]}"
-
     if n <= 20:
         ordinal_small[word] = n
     ordinal_numbers[word] = n
@@ -68,8 +53,9 @@ for n in range(1, 100):
 
 mod = Module()
 ctx = Context()
-mod.list("ordinals", desc="list of ordinals")
-mod.list("ordinals_small", desc="list of ordinals small (1-20)")
+
+mod.list("ordinals", desc="List of ordinals [1, 99]")
+mod.list("ordinals_small", desc="List of small ordinals [1, 20]")
 
 ctx.lists["self.ordinals"] = ordinal_numbers.keys()
 ctx.lists["self.ordinals_small"] = ordinal_small.keys()
@@ -77,11 +63,11 @@ ctx.lists["self.ordinals_small"] = ordinal_small.keys()
 
 @mod.capture(rule="{self.ordinals}")
 def ordinals(m) -> int:
-    """Returns a single ordinal as a digit"""
-    return int(ordinal_numbers[m[0]])
+    """Returns a single ordinal as a integer"""
+    return int(ordinal_numbers[m.ordinals])
 
 
 @mod.capture(rule="{self.ordinals_small}")
 def ordinals_small(m) -> int:
-    """Returns a single ordinal as a digit"""
-    return int(ordinal_numbers[m[0]])
+    """Returns a single small ordinal as a integer"""
+    return int(ordinal_numbers[m.ordinals_small])
