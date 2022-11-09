d50914beff3e43d752aa2361a1e11a4b1d705743
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-11-09 03:55:01 +0100

    Updated alphabet with stun

--- misc/keys/keys.py
+++ misc/keys/keys.py
@@ -14,7 +14,7 @@ language: sv
 # fmt: off
 
 # alphabet = "air bat cap drum each fine gust harp sit jury crunch look made near odd pit quench red sun trap urge vest whale plex yank zip".split(" ")
-alphabet = "air batt cap drum each fine gust harp ink jig kid look made near ox pit quench risk spun trap urn vest whale plex yank zip".split(" ")
+alphabet = "air batt cap drum each fine gust harp ink jig kid look made near ox pit quench risk stun trap urn vest whale plex yank zip".split(" ")
 default_digits = "zero one two three four five six seven eight nine ten eleven twelve".split(" ")
 
 mod.list("letter", desc="The spoken phonetic alphabet")
