3402be71d9766401ca1ceedb920b22d6407ddf9f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-02-18 12:42:22 +0100

    clean up

--- misc/parse_sim.py
+++ misc/parse_sim.py
@@ -1,36 +1,39 @@
-# https://github.com/pokey/wax_talon/blob/697e94add6316e6de700a3e228480a4e847baa59/parse_sim.py
 import re
-
-from talon import Module, app
-from talon_init import TALON_HOME
+from talon import Module, speech_system
 
 mod = Module()
 
 
 @mod.action_class
 class Actions:
-    def parse_sim(sim: str):
-        """Attempts to parse {sim} (the output of `sim()`) into a richer object with the phrase, grammar, path,
-        and possibly the matched rule(s).
-        """
-        results = SIM_RE.findall(sim)
+    def simulate_phrase(phrase: str) -> list:
+        """Simulate spoke phrase and return list of commands"""
+        sim = speech_system._sim(phrase)
+        return parse_sim(sim)
 
-        if not results:
-            return None
 
-        commands = []
+def parse_sim(sim: str):
+    """Attempts to parse {sim} (the output of `sim()`) into a richer object with the phrase, grammar, path,
+    and possibly the matched rule(s).
+    """
+    results = SIM_RE.findall(sim)
 
-        for str, num, phrase, path, rule in results:
-            commands.append(
-                {
-                    "num": int(num),
-                    "phrase": phrase,
-                    "path": path,
-                    "rule": rule,
-                }
-            )
+    if not results:
+        return None
 
-        return commands
+    commands = []
+
+    for _, num, phrase, path, rule in results:
+        commands.append(
+            {
+                "num": int(num),
+                "phrase": phrase,
+                "path": path,
+                "rule": rule,
+            }
+        )
+
+    return commands
 
 
 SIM_RE = re.compile(r"""(\[(\d+)] "([^"]+)"\s+path: ([^\n]+)\s+rule: "([^"]+))+""")
--- misc/pretty_print_phrase.py
+++ misc/pretty_print_phrase.py
@@ -3,6 +3,33 @@ from talon import Module
 mod = Module()
 
 
+@mod.action_class
+class Actions:
+    def pretty_print_phrase(phrase_str: str, commands: list):
+        """Pretty prints the command information to the terminal"""
+
+        print(f"{bcolors.ENDC}=============================={bcolors.ENDC}")
+        print(
+            f"{bcolors.BOLD}Command:{bcolors.ENDC} {bcolors.GREEN}{bcolors.BOLD}{phrase_str}{bcolors.ENDC}"
+        )
+
+        # Render the individual commands nicely.
+        for cmd in commands:
+            rule = cmd["rule"] if "rule" in cmd else ""
+            print(
+                " "
+                + " ".join(
+                    [
+                        f"#{cmd['num']}:",
+                        f"{bcolors.BOLD}{cmd['phrase']}{bcolors.ENDC}:",
+                        f"{cmd['path']} {bcolors.GREEN}{rule}{bcolors.ENDC}",
+                    ]
+                ).strip()
+            )
+
+        print(f"{bcolors.ENDC}=============================={bcolors.ENDC}")
+
+
 class bcolors:
     HEADER = "\033[95m"
     OKBLUE = "\033[94m"
@@ -13,38 +40,5 @@ class bcolors:
     ENDC = "\033[0m"
     BOLD = "\033[1m"
     UNDERLINE = "\033[4m"
-
     RED = "\033[31m"
     GREEN = "\033[92m"
-
-
-@mod.action_class
-class Actions:
-    def pretty_print_phrase(phrase_str: str, sim: str, commands: list):
-        """Pretty prints the command information to the terminal"""
-
-        print(f"{bcolors.ENDC}=============================={bcolors.ENDC}")
-        print(
-            f"{bcolors.BOLD}Command:{bcolors.ENDC} {bcolors.GREEN}{bcolors.BOLD}{phrase_str}{bcolors.ENDC}"
-        )
-
-        # Render the individual commands nicely.
-        if commands:
-            for cmd in commands:
-                rule = cmd["rule"] if "rule" in cmd else ""
-                print(
-                    " "
-                    + " ".join(
-                        [
-                            f"#{cmd['num']}:",
-                            f"{bcolors.BOLD}{cmd['phrase']}{bcolors.ENDC}:",
-                            f"{cmd['path']} {bcolors.GREEN}{rule}{bcolors.ENDC}",
-                        ]
-                    ).strip()
-                )
-        else:
-            print(
-                f"{bcolors.BOLD}{bcolors.RED}Couldn't parse sim: {bcolors.ENDC}|{sim}|"
-            )
-
-        print(f"{bcolors.ENDC}=============================={bcolors.ENDC}")
