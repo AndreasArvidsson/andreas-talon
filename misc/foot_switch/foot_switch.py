3118ff22ce817d88035ab4171628098bb51b14c4
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-02-21 08:29:18 +0100

    clean up

--- misc/foot_switch/foot_switch.py
+++ misc/foot_switch/foot_switch.py
@@ -33,7 +33,7 @@ def on_interval():
 
 
 # In a hotkey down event, eg "key(ctrl:down)", any key you press with key/insert
-# actions will be combined with ctrl since it still held. Just updating a
+# actions will be combined with ctrl since it's still held. Just updating a
 # boolean in the actual hotkey event and reading it asynchronously with cron
 # gets around this issue.
 cron.interval("16ms", on_interval)
