a748442ca8844739566b551b6d46ed5e4ae71713
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-02-24 09:32:27 +0100

    Added context for google meet

---
+++ apps/web/google_meet.py
@@ -0,0 +1,21 @@
+from talon import Context, actions, Module
+
+ctx = Context()
+mod = Module()
+
+mod.apps.google_meet = """
+tag: browser
+browser.host: meet.google.com
+"""
+
+ctx.matches = r"""
+app: google_meet
+"""
+
+ctx.tags = ["user.voip"]
+
+
+@ctx.action_class("user")
+class UserActions:
+    def mute_microphone():
+        actions.key("ctrl-d")
