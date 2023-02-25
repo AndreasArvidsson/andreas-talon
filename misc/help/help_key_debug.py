c0da7089df72e19871427431e1a537daf472fa23
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-02-25 21:25:25 +0100

    Added help key debug

---
+++ misc/help/help_key_debug.py
@@ -0,0 +1,54 @@
+from talon import Module, Context, actions, ctrl
+from ...imgui import imgui
+
+mod = Module()
+
+mod.tag("key_debug", "Keep track of which keys are held and not")
+mod.mode("help_key_debug", "Mode for showing the key debug help gui")
+
+ctx = Context()
+ctx.matches = r"""
+tag: user.key_debug
+"""
+
+held = set()
+
+
+@imgui.open(x=0)
+def gui(gui: imgui.GUI):
+    gui.header("Held keys")
+    gui.line(bold=True)
+    gui.spacer()
+    for button in ctrl.mouse_buttons_down():
+        gui.text(f"Mouse {button}")
+    for key in held:
+        gui.text(key)
+    gui.spacer()
+    if gui.button("Hide"):
+        actions.user.help_key_debug_toggle()
+
+
+@ctx.action_class("main")
+class MainActions:
+    def key(key: str):
+        actions.next(key)
+
+        for k in key.split(" "):
+            if ":down" in k:
+                held.add(k[:-5])
+            if ":up" in k:
+                k = k[:-3]
+                if k in held:
+                    held.remove(k)
+
+
+@mod.action_class
+class Actions:
+    def help_key_debug_toggle():
+        """Toggle help key debug gui"""
+        if gui.showing:
+            actions.mode.disable("user.help_key_debug")
+            gui.hide()
+        else:
+            actions.mode.enable("user.help_key_debug")
+            gui.show()
---
+++ misc/help/help_key_debug.talon
@@ -0,0 +1,6 @@
+mode: command
+mode: user.help_key_debug
+tag: user.key_debug
+-
+
+help key debug:             user.help_key_debug_toggle()
