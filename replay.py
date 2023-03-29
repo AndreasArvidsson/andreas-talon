aede522cd17641096f10aed742e4b7eb09eb2ee7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-03-29 08:36:12 +0200

    Added more results

--- replay.py
+++ replay.py
@@ -15,7 +15,7 @@ def replay_files(filenames):
     speech_system.register("pre:phrase", on_phrase)
 
     print("----------------------------")
-    print("conformer-d".ljust(30), end="")
+    print("hybrid-d".ljust(30), end="")
     print("conformer-b")
 
     for file in filenames:
@@ -41,7 +41,7 @@ def replay_dir(dir):
 # )
 
 # 2023-03-21 17:24:46    IO ----------------------------
-# 2023-03-21 17:24:46    IO conformer-d                   conformer-b
+# 2023-03-21 17:24:46    IO hybrid -d                         conformer-b
 # 2023-03-21 17:24:46    IO air                               arrow
 # 2023-03-21 17:24:46    IO bring id then bang fine           bring item pink fine
 # 2023-03-21 17:24:46    IO cap                               clippy
@@ -67,3 +67,31 @@ def replay_dir(dir):
 # 2023-03-21 17:24:47    IO wipe                              righter
 # 2023-03-21 17:24:47    IO wipe                              righter
 # 2023-03-21 17:24:47    IO word green dot                    word green dot
+
+# 2023-03-29 07:43:25    IO ----------------------------
+# 2023-03-29 07:43:25    IO hybrid -d                         hybrid -d, new acoustic file
+# 2023-03-29 07:43:25    IO air                               air
+# 2023-03-29 07:43:25    IO bring id then bang fine           bring id then bang fine
+# 2023-03-29 07:43:26    IO cap                               cap
+# 2023-03-29 07:43:26    IO clear core                        clear core
+# 2023-03-29 07:43:26    IO deli                              deli two
+# 2023-03-29 07:43:26    IO dot                               dot
+# 2023-03-29 07:43:26    IO dotted file                       git add file
+# 2023-03-29 07:43:26    IO enter                             righter
+# 2023-03-29 07:43:26    IO enter                             righter
+# 2023-03-29 07:43:26    IO gust                              jest
+# 2023-03-29 07:43:26    IO ink two dot                       ink two dot
+# 2023-03-29 07:43:27    IO jig one pit pit                   jig one pit pit
+# 2023-03-29 07:43:27    IO leap it                           leap it
+# 2023-03-29 07:43:27    IO pit                               pit
+# 2023-03-29 07:43:27    IO pit                               batt
+# 2023-03-29 07:43:27    IO round                             round
+# 2023-03-29 07:43:27    IO sam word rejection                say word rejection
+# 2023-03-29 07:43:27    IO say that tip                      say that tip
+# 2023-03-29 07:43:27    IO snake red image                   snake read image
+# 2023-03-29 07:43:27    IO string that up                    say that up
+# 2023-03-29 07:43:27    IO three                             troll
+# 2023-03-29 07:43:28    IO twin                              twin
+# 2023-03-29 07:43:28    IO wipe                              righter
+# 2023-03-29 07:43:28    IO wipe                              righter
+# 2023-03-29 07:43:28    IO word green dot                    word green dot
