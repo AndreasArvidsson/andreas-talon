8502fc5b2cc1e7656b5f2474132111a8dc015b70
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-04-30 17:30:14 +0200

    Added phrase level divider to command history

--- misc/command_history/command_history.py
+++ misc/command_history/command_history.py
@@ -20,15 +20,20 @@ class HistoryEntry:
     phrase: str
     actions: list
     ttl: int
+    phrase_start: bool
 
 
 @imgui.open(x=ui.main_screen().x, y=ui.main_screen().y)
 def gui(gui: imgui.GUI):
     t = time.monotonic()
+    add_line = False
     # Hide entries outside of display size
     for entry in history[-display_size:]:
         # If ttl is disabled or time hasn't passed yet: show command.
         if not use_ttl or entry.ttl < 0 or entry.ttl >= t:
+            if add_line and entry.phrase_start:
+                gui.line()
+            add_line = True
             gui.header(entry.phrase)
             for action in entry.actions:
                 gui.text(f"  {action.get_explanation_or_desc()}")
@@ -41,8 +46,8 @@ class Actions:
         global history
         ttl = ttl_setting.get()
         ttl = time.monotonic() + ttl if ttl > -1 else -1
-        for cmd in analyzed_phrase.commands:
-            history.append(HistoryEntry(cmd.phrase, cmd.actions, ttl))
+        for i, cmd in enumerate(analyzed_phrase.commands):
+            history.append(HistoryEntry(cmd.phrase, cmd.actions, ttl, i == 0))
         history = history[-size_setting.get() :]
 
     def command_history_toggle():
