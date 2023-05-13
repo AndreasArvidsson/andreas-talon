a63429e8044f9a4e0f58421e4a631056d6d373ff
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-05-13 10:41:49 +0200

    Added more updates event to indicator

--- misc/modes/mode_indicator.py
+++ misc/modes/mode_indicator.py
@@ -5,69 +5,128 @@ from talon.skia.imagefilter import ImageFilter
 from talon.screen import Screen
 from talon.ui import Rect
 
-mod = Module()
-setting = mod.setting("mode_indicator_show", bool, default=False)
-current_color = ""
+prefix = "mode_indicator"
+current_mode = ""
 canvas: Canvas = None
+mod = Module()
+
+setting_show = mod.setting(
+    f"{prefix}_show",
+    bool,
+    default=False,
+)
+setting_color_sleep = mod.setting(
+    f"{prefix}_color_sleep",
+    str,
+    default="808080",  # Grey
+)
+setting_color_dictation = mod.setting(
+    f"{prefix}_color_dictation",
+    str,
+    default="da70d6",  # Orchid
+)
+setting_color_mixed = mod.setting(
+    f"{prefix}_color_mixed",
+    str,
+    default="6b8e23",  # OliveDrab
+)
+setting_color_other = mod.setting(
+    f"{prefix}_color_other",
+    str,
+    default="f8f8ff",  # GhostWhite
+)
+setting_color_alpha = mod.setting(
+    f"{prefix}_color_alpha",
+    str,
+    default="aa",
+)
+
+setting_paths = {
+    s.path
+    for s in [
+        setting_show,
+        setting_color_sleep,
+        setting_color_dictation,
+        setting_color_mixed,
+        setting_color_other,
+        setting_color_alpha,
+    ]
+}
 
 
 def get_color() -> str:
-    modes = scope.get("mode")
-    if "sleep" in modes:
-        return "808080"  # Grey
-    if "dictation" in modes:
-        # Mixed mode
-        if "command" in modes:
-            return "6b8e23"  # OliveDrab
-        return "da70d6"  # Orchid
-    return "f8f8ff"  # GhostWhite
+    if current_mode == "sleep":
+        color = setting_color_sleep.get()
+    elif current_mode == "dictation":
+        color = setting_color_dictation.get()
+    elif current_mode == "mixed":
+        color = setting_color_mixed.get()
+    else:
+        color = setting_color_other.get()
+    return color + setting_color_alpha.get()
 
 
-def draw(c: SkiaCanvas):
+def on_draw(c: SkiaCanvas):
     c.paint.style = c.paint.Style.FILL
-    c.paint.color = current_color + "aa"
+    c.paint.color = get_color()
     c.paint.imagefilter = ImageFilter.drop_shadow(1, 1, 1, 1, "000000")
     radius = c.rect.height / 2 - 2
     c.draw_circle(c.rect.center.x, c.rect.top + radius, radius)
 
 
-def open():
+def show_indicator():
     global canvas
     screen: Screen = ui.main_screen()
     radius = screen.dpi / 10
     side = radius * 2
     rect = Rect(screen.rect.center.x - radius, screen.rect.top, side, side)
     canvas = Canvas.from_rect(rect)
-    canvas.register("draw", draw)
-    canvas.freeze()
+    canvas.register("draw", on_draw)
 
 
-def close():
+def hide_indicator():
     global canvas
-    canvas.unregister("draw", draw)
+    canvas.unregister("draw", on_draw)
     canvas.close()
     canvas = None
 
 
-def update_current_color():
-    global current_color
-    mode = get_color()
-    if current_color != mode:
-        current_color = mode
+def update_indicator():
+    if setting_show.get():
+        if not canvas:
+            show_indicator()
         canvas.freeze()
+    elif canvas:
+        hide_indicator()
 
 
-def update_contexts():
-    if setting.get():
-        if canvas is None:
-            open()
-        update_current_color()
-    elif canvas is not None:
-        close()
+def on_update_contexts():
+    global current_mode
+    modes = scope.get("mode")
+    if "sleep" in modes:
+        mode = "sleep"
+    elif "dictation" in modes:
+        if "command" in modes:
+            mode = "mixed"
+        else:
+            mode = "dictation"
+    else:
+        mode = "other"
+
+    if current_mode != mode:
+        current_mode = mode
+        update_indicator()
+
+
+def on_update_settings(updated_settings: set[str]):
+    if setting_paths & updated_settings:
+        update_indicator()
 
 
 def on_ready():
-    registry.register("update_contexts", update_contexts)
+    registry.register("update_contexts", on_update_contexts)
+    registry.register("update_settings", on_update_settings)
+    ui.register("screen_change", update_indicator)
 
 
 app.register("ready", on_ready)
