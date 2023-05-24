5632930b05280d739924ea1831e626ea39eb9372
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-05-24 18:53:34 +0200

    cleanup

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -1,21 +1,20 @@
 from talon import Module, actions, cron
 
 mod = Module()
-
 multiplier = 3
+cron_job = None
 _x = 0
 _y = 0
-cron_job = None
 
 
 @mod.action_class
 class Actions:
     def gamepad_scroll(x: float, y: float):
-        """Perform game pad scrolling"""
+        """Perform gamepad scrolling"""
         global cron_job, _x, _y
         _x = x * multiplier
         _y = y * -1 * multiplier
-        print(x, y)
+
         if _x != 0 or _y != 0:
             if cron_job is None:
                 cron_job = cron.interval("16ms", scroll_continuous_helper)
