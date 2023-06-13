c855b274a84ab12293780a49786ae4da7d20e2d2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-06-13 14:39:09 +0200

    Return to using web speech for Swedish dictation

--- core/engines.py
+++ core/engines.py
@@ -1,15 +1,15 @@
 from talon import Module, Context, speech_system
-from talon.engines.vosk import VoskEngine
 
-# from talon.engines.webspeech import WebSpeechEngine
-
-# speech_system.add_engine(
-#     WebSpeechEngine(),
-# )
+# from talon.engines.vosk import VoskEngine
+from talon.engines.webspeech import WebSpeechEngine
 
 speech_system.add_engine(
-    VoskEngine(model="vosk-model-small-sv-rhasspy-0.15", language="sv_SE"),
+    WebSpeechEngine(),
 )
+
+# speech_system.add_engine(
+#     VoskEngine(model="vosk-model-small-sv-rhasspy-0.15", language="sv_SE"),
+# )
 
 mod = Module()
 mod.tag("swedish", "Use voice engine for Swedish")
@@ -21,7 +21,7 @@ tag: user.swedish
 """
 
 ctx_sv.settings = {
-    # "speech.engine": "webspeech",
-    "speech.engine": "vosk",
+    "speech.engine": "webspeech",
+    # "speech.engine": "vosk",
     "speech.language": "sv_SE",
 }
