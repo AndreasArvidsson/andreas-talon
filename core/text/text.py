bb9495212094c05169ef4aa99c94534f1b1a2110
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-06-18 20:53:30 +0200

    Added spam as a phrase ender

--- core/text/text.py
+++ core/text/text.py
@@ -6,8 +6,9 @@ ctx = Context()
 
 mod.list("phrase_ender", desc="List of commands that can be used to end a phrase")
 ctx.lists["self.phrase_ender"] = {
-    "void": " ",
-    "slap": "\n",
-    "question": "?",
     "over": "",
+    "question": "?",
+    "slap": "\n",
+    "spam": ", ",
+    "void": " ",
 }
