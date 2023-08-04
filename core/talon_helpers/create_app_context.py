761bde8ac35e4724814dd1cd9b9ffd2f0b9ccf76
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-08-04 16:59:29 +0200

    update create app context

--- core/talon_helpers/create_app_context.py
+++ core/talon_helpers/create_app_context.py
@@ -1,9 +1,9 @@
-from talon import Module, actions, app, ui
+import os
+import re
 from itertools import islice
 from pathlib import Path
-import re
-import os
 
+from talon import Module, actions, app, ui
 
 APPS_DIR = Path(__file__).parent.parent.parent / "apps"
 
@@ -15,24 +15,20 @@ class Actions:
     def talon_create_app_context():
         """Create a new python context file for the current application"""
         active_app = ui.active_app()
-        app_name = create_name(active_app.name)
+        app_name = get_app_name(active_app.name)
         app_dir = APPS_DIR / app_name
+        filename = get_filename(app_name)
         talon_file = app_dir / f"{app_name}.talon"
-        python_file = app_dir / f"{app_name}.py"
-
-        if app_dir.is_dir():
-            raise OSError(f"Application directory '{app_name}' already exists")
+        python_file = app_dir / f"{filename}.py"
 
         talon_context = get_talon_context(app_name)
         python_context = get_python_context(active_app, app_name)
 
-        os.mkdir(app_dir)
+        if not app_dir.is_dir():
+            os.mkdir(app_dir)
 
-        with open(talon_file, "w", encoding="utf-8") as file:
-            file.write(talon_context)
-
-        with open(python_file, "w", encoding="utf-8") as file:
-            file.write(python_context)
+        create_file(talon_file, talon_context)
+        create_file(python_file, python_context)
 
 
 def get_python_context(active_app: ui.App, app_name: str) -> str:
@@ -61,11 +57,15 @@ app: {app_name}
 
 
 def get_talon_context(app_name: str) -> str:
-    return f"""\
-app: {app_name}
+    return f"""app: {app_name}
 -
 
 """
+
+
+def get_filename(app_name: str) -> str:
+    platform = app.platform if app.platform != "windows" else "win"
+    return f"{app_name}_{platform}"
 
 
 def get_app_context(active_app: ui.App) -> str:
@@ -76,8 +76,17 @@ def get_app_context(active_app: ui.App) -> str:
     return f"app.name: {active_app.name}"
 
 
-def create_name(text: str, max_len=20) -> str:
+def get_app_name(text: str, max_len=20) -> str:
     pattern = re.compile(r"[A-Z][a-z]*|[a-z]+|\d")
     return "_".join(
         list(islice(pattern.findall(text.replace(".exe", "")), max_len))
     ).lower()
+
+
+def create_file(path: Path, content: str):
+    if path.is_file():
+        print(f"Application context file '{path}' already exists")
+        return
+
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(content)
