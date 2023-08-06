bd29f6b9eb7678be26bdafbc514902de69abe6de
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-08-06 13:04:05 +0200

    revert vscode insert new line below implementation

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -131,9 +131,9 @@ class EditActions:
     def line_insert_up():
         actions.user.vscode("editor.action.insertLineBefore")
 
-    def line_insert_down():
-        # Don't use RPC since some vscode extension(eg markdown) has specific behavior on enter
-        actions.key("ctrl-enter")
+    # Don't use RPC since some vscode extension(eg markdown) has specific behavior on enter
+    # def line_insert_down():
+        # actions.user.vscode("editor.action.insertLineAfter")
 
     def delete_line():
         actions.user.vscode("editor.action.deleteLines")
