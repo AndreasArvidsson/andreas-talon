a6b2f631d78ce895dd12427e3d091eb770157bed
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-09-02 16:47:52 +0200

    Use vscode snippet insertion instead of cursorless

--- apps/vscode/vscode.py
+++ apps/vscode/vscode.py
@@ -250,7 +250,8 @@ class UserActions:
 
     # ----- Snippets -----
     def insert_snippet(snippet: str):
-        actions.user.cursorless_insert_snippet(snippet)
+        # actions.user.cursorless_insert_snippet(snippet)
+        actions.user.vscode("editor.action.insertSnippet", {"snippet": snippet})
 
 
 # @ctx_notebook.action_class("main")
