ce95aedd0efefcb82cd05d07cd6c892b761053bb
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-11-12 20:27:51 +0100

    Cleanup

--- apps/vscode/cursorless.py
+++ apps/vscode/cursorless.py
@@ -49,6 +49,7 @@ class Actions:
     def c_insert_snippet(destination: Any, name: str):
         """Insert cursorless snippet <name>"""
         snippet: Snippet = actions.user.get_snippet(name)
+
         actions.user.cursorless_insert_snippet(
             snippet.body, destination, snippet.insertion_scopes
         )
@@ -57,11 +58,12 @@ class Actions:
         """Wrap the target with snippet <id>"""
         index = id.rindex(".")
         snippet_name = id[:index]
-        var_name = id[index + 1]
+        variable_name = id[index + 1]
         snippet: Snippet = actions.user.get_snippet(snippet_name)
-        variable = snippet.get_variable_strict(var_name)
+        variable = snippet.get_variable_strict(variable_name)
+
         actions.user.cursorless_wrap_with_snippet(
-            snippet.body, target, var_name, variable.wrapper_scope
+            snippet.body, target, variable.name, variable.wrapper_scope
         )
 
 
