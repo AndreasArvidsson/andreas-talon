e979151d2c64db70ebda8533a4ee789bfe759731
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-11-29 17:06:37 +0100

    Added to do snippet command

--- languages/tags/code_comments.py
+++ languages/tags/code_comments.py
@@ -1,4 +1,13 @@
-from talon import Module
+from typing import Optional
+from talon import Module, actions
 
 mod = Module()
 mod.tag("code_comments")
+
+
+@mod.action_class
+class Actions:
+    def insert_todo_comment_snippet(message: Optional[str] = None):
+        """Inserts a TODO comment snippet"""
+        message = text = actions.user.format_text(message or "", "SENTENCE")
+        actions.user.insert_snippet_by_name("commentLine", {"0": f"TODO: {message}$0"})
--- languages/tags/code_comments.talon
+++ languages/tags/code_comments.talon
@@ -2,3 +2,6 @@ tag: user.code_comments
 -
 
 comment:                    code.toggle_comment()
+
+snip to do:                 user.insert_todo_comment_snippet()
+snip to do <user.text>:     user.insert_todo_comment_snippet(text)
