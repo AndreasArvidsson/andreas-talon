5b8bf392c322549c351de5c405b82764c941a06d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-01-09 15:10:24 +0100

    update netflix matcher

--- apps/netflix.py
+++ apps/netflix.py
@@ -6,6 +6,10 @@ mod.apps.netflix = """
 os: windows
 and app.exe: ApplicationFrameHost.exe
 and win.title: Netflix
+
+os: windows
+and tag: browser
+and browser.host: www.netflix.com
 """
 
 ctx = Context()
