086df66954d05d2be4eb54f3f35ba95f9aab6119
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-02-09 09:55:08 +0100

    Added svt play

---
+++ apps/svt_play.py
@@ -0,0 +1,18 @@
+from talon import Module, Context
+
+mod = Module()
+
+mod.apps.svt_play = """
+tag: browser
+and browser.host: www.svtplay.se
+"""
+
+ctx = Context()
+
+ctx.matches = r"""
+app: svt_play
+"""
+
+ctx.settings = {
+    "user.mode_indicator_show": False,
+}
