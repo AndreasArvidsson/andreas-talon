b2f2dbbb7753a62b132cf62e7f750bb8793b22b7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-03-11 11:41:38 +0100

    Update sound devices

--- core/sound/sound.py
+++ core/sound/sound.py
@@ -4,7 +4,6 @@ from subprocess import call
 mod = Module()
 mod.list("playback_device", "Playback devices")
 mod.list("microhpone_device", "Microphone devices")
-mod.list("playback_microphone_pair", "Playback / microphone device pair")
 
 
 @mod.action_class
@@ -19,9 +18,6 @@ class Actions:
 
     def change_sound_device(name: str):
         """Change sound device."""
-
-    def change_sound_device_pair(name: str):
-        """Change sound device pair."""
 
     def sound_microphone_enabled() -> bool:
         """Returns true if the microphone is not set to 'None'"""
@@ -50,26 +46,16 @@ ctx_win.matches = r"""
 os: windows
 """
 
-ctx_win.lists["user.playback_device"] = {
-    "Headphones": "Headphones",
-    "Speakers": "Speakers",
-    "TV": "TV",
-    "razer": "Razer headphones",
-}
+# ctx_win.lists["user.playback_device"] = {
+#     "Headphones": "Headphones",
+#     "Speakers": "Speakers",
+# }
 
 ctx_win.lists["user.microhpone_device"] = {
     "internal": "Internal_mic",
     "dpa": "DPA",
-    "razer": "Razer microphone",
     "blue yeti": "Blue Yeti",
 }
-
-playback_microphone_pair = {
-    "headphones": ["Headphones", "Headphones_mic"],
-    "solo": ["Speakers", "DPA"],
-    "razer": ["Razer headphones", "Razer microphone"],
-}
-ctx_win.lists["user.playback_microphone_pair"] = playback_microphone_pair.keys()
 
 
 @ctx_win.action_class("user")
@@ -77,12 +63,6 @@ class UserActionsWin:
     def change_sound_device(name: str):
         change_sound_device_win(name, 1)
         change_sound_device_win(name, 2)
-
-    def change_sound_device_pair(name: str):
-        pair = playback_microphone_pair[name]
-        actions.user.notify(f"Using {pair[0]} / {pair[1]}")
-        actions.user.change_sound_device(pair[0])
-        actions.user.change_sound_device(pair[1])
 
 
 def change_sound_device_win(name: str, role: int):
--- core/sound/sound.talon
+++ core/sound/sound.talon
@@ -14,6 +14,3 @@ media stop:                 key(stop)
 ^microphone {user.microhpone_device}:
     user.notify("Microphone: {microhpone_device}")
     user.change_sound_device(microhpone_device)
-
-^use {user.playback_microphone_pair}:
-    user.change_sound_device_pair(playback_microphone_pair)
