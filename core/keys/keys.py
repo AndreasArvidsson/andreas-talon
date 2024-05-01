654f350c9fd24ecf8b424e6416006ffa68aae395
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-05-01 08:23:28 +0200

    Disabled oh for zero

--- core/keys/keys.py
+++ core/keys/keys.py
@@ -27,7 +27,7 @@ default_digits = (
 mod.list("digit", "All number/digit keys")
 ctx.lists["user.digit"] = {
     **{default_digits[i]: str(i) for i in range(10)},
-    "oh": "0",
+    # "oh": "0",
 }
 
 mod.list("key_function", "All function keys")
