fe4bf33e2c490e103bcfe6809b23df9497c046d1
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-05 10:58:42 +0200

    Refactor edit insert paste into separate file

--- core/edit/edit.py
+++ core/edit/edit.py
@@ -1,32 +1,6 @@
 from talon import Context, Module, actions, clip
-import re
 
 mod = Module()
-
-# ---------- Paste insert ------
-
-
-ctx_insert_paste = Context()
-
-
-# Use paste for inserting text that cannot be undone in a single undo step
-PASTE_RE = re.compile(r"[\s/-]")
-# Use paste for inserting text with length greater or equal to the threshold
-PASTE_THRESHOLD = 10
-
-
-@ctx_insert_paste.action_class("main")
-class MainActions:
-    def insert(text: str):
-        if len(text) >= PASTE_THRESHOLD or re.search(PASTE_RE, text):
-            actions.user.paste_text(text)
-        else:
-            actions.next(text)
-
-
-# ----------------------
-
-
 ctx = Context()
 
 
