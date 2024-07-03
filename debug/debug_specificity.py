10cd1f02c27418e04d7c78ba9e19056ba501558d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-07-03 03:55:46 +0200

    Added debug specificity

---
+++ debug/debug_specificity.py
@@ -0,0 +1,45 @@
+from talon import Module, Context, registry, app, actions
+
+
+matchers = [
+    "app: vscode",
+    "tag: user.cursorless",
+    "mode: all",
+    "mode: command",
+    "not mode: sleep",
+]
+
+
+def on_ready():
+    lists = registry.lists["user.debug_matches"]
+    lists.reverse()
+    result = []
+
+    for i, l in enumerate(lists):
+        matches = l["matches"]  # type: ignore
+        result.append(f"{i+1}: {matches}")
+
+    res = "\n".join(result)
+    print(" \n" + res)
+    actions.clip.set_text(res)
+
+
+def run():
+    mod = Module()
+    mod.list("debug_matches")
+
+    for matches in matchers:
+        ctx = Context()
+        ctx.matches = matches
+        ctx.lists["user.debug_matches"] = {"matches": matches}
+
+    app.register("ready", on_ready)
+
+
+# run()
+
+# 1: app: vscode
+# 2: not mode: sleep
+# 3: mode: command
+# 4: mode: all
+# 5: tag: user.cursorless
