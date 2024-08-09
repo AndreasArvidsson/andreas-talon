21d30582437a2734565a5e07b5393769613547b3
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-09 10:33:11 +0200

    Added action to translate Swedish into english

--- core/text/translate.py
+++ core/text/translate.py
@@ -6,6 +6,7 @@ from deep_translator import GoogleTranslator
 import time
 
 en_to_sv = GoogleTranslator(source="en", target="sv")
+sv_to_en = GoogleTranslator(source="sv", target="en")
 
 
 mod = Module()
@@ -13,12 +14,18 @@ mod = Module()
 
 @mod.action_class
 class Actions:
-    def translate_english_to_swedish(english_text: str) -> str:
+    def translate_english_to_swedish(text: str) -> str:
         """Translate english text to swedish"""
-        t1 = time.perf_counter()
-        translated = en_to_sv.translate(english_text)
-        t2 = time.perf_counter()
-        actions.user.debug(
-            f"Translated {len(english_text)} characters in {round((t2-t1)*100)}ms"
-        )
-        return translated
+        return translate(en_to_sv, text)
+
+    def translate_swedish_to_english(text: str) -> str:
+        """Translate swedish text to english"""
+        return translate(sv_to_en, text)
+
+
+def translate(translator: GoogleTranslator, text: str) -> str:
+    t1 = time.perf_counter()
+    translated = translator.translate(text)
+    t2 = time.perf_counter()
+    actions.user.debug(f"Translated {len(text)} characters in {round((t2-t1)*100)}ms")
+    return translated
