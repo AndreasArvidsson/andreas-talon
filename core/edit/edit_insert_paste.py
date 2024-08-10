a1b8c9e92be1a05669446485ec5c5b0ea506843d
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-08-10 14:09:00 +0200

    Updated edit paste criteria

--- core/edit/edit_insert_paste.py
+++ core/edit/edit_insert_paste.py
@@ -14,9 +14,9 @@ not tag: user.insert_paste_disabled
 
 
 # Use paste for inserting text that cannot be undone in a single undo step
-PASTE_RE = re.compile(r"[\s/-]")
+PASTE_RE = re.compile(r"\s")
 # Use paste for inserting text with length greater or equal to the threshold
-PASTE_THRESHOLD = 10
+PASTE_THRESHOLD = 25
 
 
 @ctx.action_class("main")
