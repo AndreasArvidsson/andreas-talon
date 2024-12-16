b6c85738905d390657e118755f57daa23f2d9470
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-12-16 16:18:04 +0100

    Clippy use Talon community rpc client

--- plugins/clippy/clippy.py
+++ plugins/clippy/clippy.py
@@ -1,12 +1,12 @@
 from typing import Any, Optional
 from talon import Module, Context, actions, app, ui
 
-from ...core.rpc_client.rpc_client import RpcClient
 from .clippy_targets import ClippyPrimitiveTarget, ClippyTarget
 
-key = "cmd-shift-f18" if app.platform == "mac" else "ctrl-shift-alt-o"
+rpc_key = "cmd-shift-f18" if app.platform == "mac" else "ctrl-shift-alt-o"
+rpc_dir_name = "clippy-command-server"
+rpc_command_id = "clippy-command"
 
-rpc = RpcClient("Clippy", key)
 
 mod = Module()
 
@@ -62,13 +62,14 @@ class Actions:
 
     def clippy_command_with_targets(command_id: str, targets: list[ClippyTarget]):
         """Send a command with targets to the clipboard manager"""
+        serializable_targets = [t.to_dict() for t in targets]
         if command_id == "pasteItems":
-            command = {"id": "copyItems", "targets": targets}
+            command = {"id": "copyItems", "targets": serializable_targets}
             send(command)
             actions.sleep("50ms")
             actions.edit.paste()
         else:
-            send({"id": command_id, "targets": targets})
+            send({"id": command_id, "targets": serializable_targets})
 
     def clippy_paste_indices(indices: list[int]):
         """Paste items from the clipboard manager at the given indices"""
@@ -77,24 +78,41 @@ class Actions:
 
     def clippy_search(text: str):
         """Search for <text> in the clipboard manager"""
-        send({"id": "search", "text": text})
+        send({"id": "searchItems", "text": text})
 
     def clippy_rename(targets: list[ClippyTarget], text: Optional[str] = None):
         """Rename clipboard targets to <text>"""
-        send({"id": "renameItems", "targets": targets, "text": text})
+        serializable_targets = [t.to_dict() for t in targets]
+        send({"id": "renameItems", "targets": serializable_targets, "text": text})
 
     def clippy_get(targets: list[ClippyTarget]):
         """Get clipboard targets"""
-        result = get({"id": "getItems", "targets": targets})
+        serializable_targets = [t.to_dict() for t in targets]
+        result = get({"id": "getItems", "targets": serializable_targets})
         print(result)
 
 
 def send(command: Any):
-    rpc.send(command, wait_for_finish=True)
+    return rpc_command(command, wait_for_finish=True)
 
 
 def get(command: Any):
-    return rpc.send(command, return_command_output=True)
+    return rpc_command(command, return_command_output=True)
+
+
+def rpc_command(
+    command: Any,
+    wait_for_finish: bool = False,
+    return_command_output: bool = False,
+):
+    return actions.user.rpc_client_run_command(
+        rpc_dir_name,
+        lambda: actions.key(rpc_key),
+        rpc_command_id,
+        [command],
+        wait_for_finish,
+        return_command_output,
+    )
 
 
 def update(window, onShow):
