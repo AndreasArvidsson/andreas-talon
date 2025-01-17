ee3f02bf6a9dc350f806200dd4efdba952d502d2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-01-17 08:36:48 +0100

    Clean up

--- plugins/help/help_adhoc.py
+++ plugins/help/help_adhoc.py
@@ -4,7 +4,8 @@ from ...core.imgui import imgui
 
 @imgui.open(x=0, y=0)
 def gui(gui: imgui.GUI):
-    languages = scope.data["main"]["language"]
+    languages = list(scope.data["main"]["language"])
+    languages.sort()
     for l in languages:
         gui.text(l)
 
