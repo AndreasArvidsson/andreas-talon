58f17dd7467f924bcc0f28bb0be95af6be4ee990
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-07-09 16:11:41 +0200

    Clean up types in code operators

--- languages/tags/code_operators.py
+++ languages/tags/code_operators.py
@@ -5,41 +5,43 @@ mod = Module()
 mod.tag("code_operators", "Enable code operators")
 mod.list("code_operator", "List of code operators")
 
+Operator = str | None
+
 
 @dataclass
 class CodeOperators(dict):
     # ----- Assignment operator -----
-    op_assign: str = None
+    op_assign: Operator = None
 
     # ----- Math operators -----
-    op_sub: str = None
-    op_sub_assign: str = None
-    op_add: str = None
-    op_add_assign: str = None
-    op_mult: str = None
-    op_mult_assign: str = None
-    op_div: str = None
-    op_div_assign: str = None
-    op_mod: str = None
-    op_mod_assign: str = None
-    op_pow: str = None
+    op_sub: Operator = None
+    op_sub_assign: Operator = None
+    op_add: Operator = None
+    op_add_assign: Operator = None
+    op_mult: Operator = None
+    op_mult_assign: Operator = None
+    op_div: Operator = None
+    op_div_assign: Operator = None
+    op_mod: Operator = None
+    op_mod_assign: Operator = None
+    op_pow: Operator = None
 
     # ----- Comparison operators -----
-    is_equal: str = None
-    is_not_equal: str = None
-    is_less: str = None
-    is_greater: str = None
-    is_less_equal: str = None
-    is_greater_equal: str = None
-    is_not: str = None
-    is_null: str = None
-    is_not_null: str = None
-    is_in: str = None
+    is_equal: Operator = None
+    is_not_equal: Operator = None
+    is_less: Operator = None
+    is_greater: Operator = None
+    is_less_equal: Operator = None
+    is_greater_equal: Operator = None
+    is_not: Operator = None
+    is_null: Operator = None
+    is_not_null: Operator = None
+    is_in: Operator = None
 
     # ----- Logical operators -----
-    op_in: str = None
-    op_and: str = None
-    op_or: str = None
+    op_in: Operator = None
+    op_and: Operator = None
+    op_or: Operator = None
 
     def __post_init__(self):
         for field in fields(self):
