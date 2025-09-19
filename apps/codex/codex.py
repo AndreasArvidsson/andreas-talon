f1da541752715473e83cd966f9064a938555f407
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-09-19 15:52:41 +0200

    Added context specific insert new line to codex

--- apps/codex/codex.py
+++ apps/codex/codex.py
@@ -1,5 +1,16 @@
-from talon import Module
+from talon import Module, Context, actions
 
 
 mod = Module()
 mod.tag("codex")
+
+ctx = Context()
+ctx.matches = r"""
+tag: user.codex
+"""
+
+
+@ctx.action_class("edit")
+class Actions:
+    def line_insert_down():
+        actions.key("ctrl-j")
