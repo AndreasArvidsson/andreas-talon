04d3658cbfaafc2a9efb1e606cc927697eadfdcd
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-02-21 12:17:03 +0100

    Make conditional wake up robust against eye tracker failure

--- core/modes/conditional_wake_up.py
+++ core/modes/conditional_wake_up.py
@@ -1,7 +1,9 @@
 from talon import Module, cron, actions, tracking_system
 
 mod = Module()
-gaze_detected = False
+
+# True = gaze detected, False = no gaze detected, None = No gaze events
+gaze_detected: bool | None
 
 
 def on_gaze(e):
@@ -11,10 +13,17 @@ def on_gaze(e):
     if x or y:
         gaze_detected = True
         tracking_system.unregister("gaze", on_gaze)
+    else:
+        gaze_detected = False
 
 
 def evaluate_gaze_detection():
     tracking_system.unregister("gaze", on_gaze)
+
+    # Didn't get any gaze events from eye tracker.
+    # This can happen if the eye tracker is not working properly.
+    if gaze_detected is None:
+        return
 
     if not gaze_detected:
         print("No gaze detected, putting Talon to sleep")
@@ -31,6 +40,6 @@ class Actions:
         if not tracking_system.trackers:
             return
 
-        gaze_detected = False
+        gaze_detected = None
         tracking_system.register("gaze", on_gaze)
         cron.after("1s", evaluate_gaze_detection)
