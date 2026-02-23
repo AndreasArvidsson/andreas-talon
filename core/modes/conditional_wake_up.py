47438a09f9d5eaf44addbaeef32c2f547bce6bc7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-02-23 12:03:39 +0100

    Clean up eye_tracker_detect_gaze_or_sleep implementation

--- core/modes/conditional_wake_up.py
+++ core/modes/conditional_wake_up.py
@@ -1,28 +1,27 @@
 from talon import Module, cron, actions, tracking_system
 
-mod = Module()
 
-# True = gaze detected, False = no gaze detected, None = No gaze events
-gaze_detected: bool | None
+mod = Module()
+gaze_detected: bool
+event_triggered: bool
 
 
 def on_gaze(e):
-    global gaze_detected
-    x, y = e.gaze
+    global gaze_detected, event_triggered
 
-    if x or y:
+    event_triggered = True
+
+    if not e.gaze.zero():
         gaze_detected = True
         tracking_system.unregister("gaze", on_gaze)
-    else:
-        gaze_detected = False
 
 
 def evaluate_gaze_detection():
     tracking_system.unregister("gaze", on_gaze)
 
-    # Didn't get any gaze events from eye tracker.
+    # Didn't get any gaze events from the eye tracker.
     # This can happen if the eye tracker is not working properly.
-    if gaze_detected is None:
+    if not event_triggered:
         return
 
     if not gaze_detected:
@@ -34,12 +33,15 @@ def evaluate_gaze_detection():
 class Actions:
     def eye_tracker_detect_gaze_or_sleep():
         """Put Talon to sleep if no gaze is detected, otherwise do nothing"""
-        global gaze_detected
+        global gaze_detected, event_triggered
 
         # No eye trackers are connected, do nothing
         if not tracking_system.trackers:
             return
 
-        gaze_detected = None
+        gaze_detected = False
+        event_triggered = True
+
         tracking_system.register("gaze", on_gaze)
+
         cron.after("1s", evaluate_gaze_detection)
