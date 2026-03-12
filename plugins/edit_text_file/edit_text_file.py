8448cccb5c46f0d21fef26303c360548cbf4d2cf
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-03-12 22:01:28 +0100

    Cleanup

--- plugins/edit_text_file/edit_text_file.py
+++ plugins/edit_text_file/edit_text_file.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
-from talon import Context, Module, actions
+
+from talon import Module, actions
 
 REPO_DIR = Path(__file__).parent.parent.parent
 
@@ -11,21 +12,12 @@ mod.list(
     desc="Paths to frequently edited files (Talon list, CSV, etc.)",
 )
 
-ctx_win = Context()
-ctx_win.matches = "os: windows"
-
 
 @mod.action_class
 class Actions:
     @staticmethod
     def edit_text_file(file: str):
-        """Tries to open a file in the user's preferred text editor."""
-
-
-@ctx_win.action_class("user")
-class WinActions:
-    @staticmethod
-    def edit_text_file(file: str):
+        """Edit file in the user's preferred text editor"""
         path = get_full_path(file)
         actions.user.exec(f"code {path}")
         actions.user.wait_for_title_change()
