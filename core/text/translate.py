6228ec6aab07af779fb6aa4c0cdf115a2106915e
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-06-23 10:15:13 +0200

    Updated translate.py to not depend on deep_translator import anymore

--- core/text/translate.py
+++ core/text/translate.py
@@ -1,13 +1,8 @@
-# linux: `TALON_HOME/bin/pip install -U deep-translator`
-# win:   `TALON_HOME/venv/3.11/Scripts/pip.bat install -U deep-translator`
-
-from talon import Module, actions
-from deep_translator import GoogleTranslator
+import json
 import time
-
-en_to_sv = GoogleTranslator(source="en", target="sv")
-sv_to_en = GoogleTranslator(source="sv", target="en")
-
+from urllib.parse import urlencode
+from urllib.request import urlopen
+from talon import Module, actions
 
 mod = Module()
 
@@ -17,19 +12,43 @@ class Actions:
     @staticmethod
     def translate_english_to_swedish(text: str) -> str:
         """Translate english text to swedish"""
-        return translate(en_to_sv, text)
+        return translate("en", "sv", text)
 
     @staticmethod
     def translate_swedish_to_english(text: str) -> str:
         """Translate swedish text to english"""
-        return translate(sv_to_en, text)
+        return translate("sv", "en", text)
 
 
-def translate(translator: GoogleTranslator, text: str) -> str:
+def translate(source_lang: str, target_lang: str, text: str) -> str:
     t1 = time.perf_counter()
-    translated = translator.translate(text)
+    translated = google_translate(source_lang, target_lang, text)
     t2 = time.perf_counter()
     actions.user.debug(
-        f"Translated {len(text)} characters in {round((t2 - t1) * 100)}ms"
+        f"Translated {len(text)} characters in {round((t2 - t1) * 1000)}ms"
     )
     return translated
+
+
+def google_translate(source_lang: str, target_lang: str, text: str) -> str:
+    query = urlencode(
+        {
+            # The "client" parameter is set to "gtx" to indicate that this request is coming from a Google Translate client.
+            "client": "gtx",
+            # The "dt" parameter is set to "t" to specify that we want the translated text in the response.
+            "dt": "t",
+            # The "sl" parameter is set to the source language code (e.g., "en" for English) to specify the language of the input text.
+            "sl": source_lang,
+            # The "tl" parameter is set to the target language code (e.g., "sv" for Swedish) to specify the language we want the text translated into.
+            "tl": target_lang,
+            # The "q" parameter is set to the text we want to translate.
+            "q": text,
+        }
+    )
+
+    url = f"https://translate.googleapis.com/translate_a/single?{query}"
+
+    with urlopen(url, timeout=3) as response:
+        data = json.loads(response.read().decode("utf-8"))
+
+    return "".join(part[0] for part in data[0] if part[0])
