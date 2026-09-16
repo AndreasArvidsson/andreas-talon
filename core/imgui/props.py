fa7f819f7d0363f6e049c657a2e70af067e66e69
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2026-09-16 12:01:16 +0200

    Use full Callable types

--- core/imgui/props.py
+++ core/imgui/props.py
@@ -1,12 +1,16 @@
 from collections.abc import Callable
 from dataclasses import dataclass
+from typing import TYPE_CHECKING
 
 from talon.screen import Screen
+
+if TYPE_CHECKING:
+    from .gui import GUI
 
 
 @dataclass
 class Props:
-    callback: Callable
+    callback: Callable[["GUI"], None]
     screen: Screen | None
     x: float | None
     y: float | None
